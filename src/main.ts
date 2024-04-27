9d32acc0564d6dd3bba91006030c034fd4d2095e
Antoine Chemin <67341298+AntoineCheminPro@users.noreply.github.com>
2024-04-27 15:20:51 +0200

    header & http done

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { DataService } from './app/core/services/data.service';
+import { OlympicService } from './app/core/services/olympic.service';
+import { HttpClientModule } from '@angular/common/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    DataService,
+    OlympicService,
+  ]
+}).catch(err => console.error(err));
