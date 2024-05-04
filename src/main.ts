d8e8f120a1996634b76db74312389aa14eaa9e1f
Antoine Chemin <67341298+AntoineCheminPro@users.noreply.github.com>
2024-05-04 14:00:58 +0200

    before install ngcharts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { DataService } from './app/core/services/data.service';
 import { OlympicService } from './app/core/services/olympic.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -16,7 +12,7 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
-    DataService,
+
     OlympicService,
   ]
 }).catch(err => console.error(err));
